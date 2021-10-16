ae4082f8be2655690e0361a2ed6f2433b2e94a1d
Davinci Lisapaly <davincilisapaly@visionplanner.com>
2021-10-16 19:22:14 +0200

    Added mainview

---
+++ View/MainView.py
@@ -0,0 +1,14 @@
+from termcolor import colored
+
+from Service.AdvisorService import AdvisorService
+
+class MainView:
+  def __init__(self, advisorService):
+    self.advisorService = advisorService
+
+  def GetMenu(self):
+   return [ [1, 'update password for advisor', self.advisorService.UpdatePasswordForAdvisor]]
+  #           [3, 'add new client', clientService.add_new_client], [4, 'add new user', userService.add_new_user], \
+  #           [5, 'make a user "admin"', userService.make_a_user_admin], \
+  #           [6, 'delete a client', clientService.delete_client], [7, 'delete a user', userService.delete_user], \
+  #           [8, 'change password', userService.change_password], [0, 'logout', loginService.logout]]
